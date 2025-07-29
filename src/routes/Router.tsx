37625254cca50976a15814aec8504ea7ed59f338
이가은 <gelee@iga-eun-ui-MacBookPro.local>
2025-07-29 13:36:21 +0900

    feat: 기본 레이아웃 생성 및 router 설정

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,12 +1,36 @@
 import { createBrowserRouter } from 'react-router-dom';
+import DefaultLayout from '../components/layout/default/Layout.tsx';
 import { Home } from '../pages/Home/Home.tsx';
+import { Category } from '../pages/Category/Category.tsx';
+import { Meeting } from '../pages/Meeting/Meeting.tsx';
+import { Mypage } from '../pages/Mypage/Mypage.tsx';
 import { ComponentGallery } from '../pages/ComponentGallery/ComponentGallery.tsx';
 
 export const router = createBrowserRouter([
   {
+    // 레이아웃이 적용되는 라우트들
     path: '/',
-    element: <Home />,
+    element: <DefaultLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: 'category',
+        element: <Category />,
+      },
+      {
+        path: 'meeting',
+        element: <Meeting />,
+      },
+      {
+        path: 'mypage',
+        element: <Mypage />,
+      },
+    ],
   },
+  // 레이아웃이 적용되지 않는 라우트들
   {
     path: '/components',
     element: <ComponentGallery />,
