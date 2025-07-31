2f36be2ec63143ef065ef73d83bb8c83beb29d45
이가은 <gelee@iga-eun-ui-MacBookPro.local>
2025-07-31 10:36:46 +0900

    fix: notice 라우터 연결

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -3,19 +3,20 @@ import { createBrowserRouter } from 'react-router-dom';
 // layout
 import DefaultLayout from '../components/layout/default/Layout.tsx';
 import SearchLayout from '../components/layout/search/Layout.tsx';
+import TitleLayout from '../components/layout/title/Layout.tsx';
 
 // page
 import { Home } from '../pages/Home/Home.tsx';
 import { Category } from '../pages/Category/Category.tsx';
 import { Meeting } from '../pages/Meeting/Meeting.tsx';
+import { MeetingDetail } from '../pages/Meeting/MeetingDetail.tsx';
 import { Mypage } from '../pages/Mypage/Mypage.tsx';
 import { Search } from '../pages/Search.tsx';
 import { Login } from '../pages/Login.tsx';
 import { Signup } from '../pages/Signup/Signup.tsx';
+import { Notice } from '../pages/NotificationList.tsx';
 
 import { ComponentGallery } from '../pages/ComponentGallery/ComponentGallery.tsx';
-import  NotificationList from '../pages/NotificationList.tsx';
-import { MeetingDetail } from '../pages/Meeting/MeetingDetail.tsx';
 
 export const router = createBrowserRouter([
   // [기본] 레이아웃이 적용되는 라우트들
@@ -58,6 +59,18 @@ export const router = createBrowserRouter([
     ]
   },
 
+  // [타이틀] 레이아웃이 적용되는 라우트들
+  {
+    path: '/',
+    element: <TitleLayout />,
+    children: [
+      {
+        path: 'notice',
+        element: <Notice />,
+      },
+    ]
+  },
+
   // 레이아웃이 적용되지 않는 라우트들
   {
     path: '/login',
@@ -70,10 +83,5 @@ export const router = createBrowserRouter([
   {
     path: '/components',
     element: <ComponentGallery />,
-  },
-
-  { 
-    path: '/notificaionList',
-    element: <NotificationList />,
   },
 ]);
