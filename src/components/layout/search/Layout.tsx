6f87e80f26dc73f8fd5a6b5fe4485c193193e148
황인찬 <hwang-inchan@hwang-inchan-ui-MacBookPro.local>
2025-08-08 13:33:06 +0900

    style: placeholder 2글자 이상 제거

--- src/components/layout/search/Layout.tsx
+++ src/components/layout/search/Layout.tsx
@@ -13,7 +13,7 @@ export default function SearchLayout() {
     <div className="h-screen flex flex-col overflow-hidden">
       {/* 고정 헤더 - 높이를 명시적으로 설정 */}
       <div className="h-14 flex-shrink-0">
-        <Header placeholder="모임을 검색하세요(2글자 이상)" onSearch={handleSearch} />
+        <Header placeholder="모임을 검색하세요" onSearch={handleSearch} />
       </div>
 
       {/* 스크롤 가능한 메인 영역 */}
